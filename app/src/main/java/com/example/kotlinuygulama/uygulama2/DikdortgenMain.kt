b54d758de718f914011962ef035ade6423d1b706
Ahmet GÜR <ahmetgur190758@gmail.com>
2023-07-26 20:17:26 +0300

    Kotlin examples.

--- app/src/main/java/com/example/kotlinuygulama/uygulama2/DikdortgenMain.kt
+++ app/src/main/java/com/example/kotlinuygulama/uygulama2/DikdortgenMain.kt
@@ -1,10 +1,9 @@
 package com.example.kotlinuygulama.uygulama2
 
 fun main() {
-    val dikdortgen1 = Dikdortgen(10.0, 2.0)
-
-    val alanDikdortgen = dikdortgen1.alan()
-    val cevreDikdortgen = dikdortgen1.cevre()
+    val dikdortgen = Dikdortgen(10.0, 2.0)
+    val alanDikdortgen = dikdortgen.alan()
+    val cevreDikdortgen = dikdortgen.cevre()
 
     println("alan: $alanDikdortgen")
     println("cevre: $cevreDikdortgen")
