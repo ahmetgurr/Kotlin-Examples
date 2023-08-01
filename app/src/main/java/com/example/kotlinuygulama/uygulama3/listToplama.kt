54bfd815ecbb2a561ffd8939fda66bcf4cf245f9
Ahmet GÜR <ahmetgur190758@gmail.com>
2023-08-01 20:33:56 +0300

    Kotlin examples week3.

---
+++ app/src/main/java/com/example/kotlinuygulama/uygulama3/listToplama.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinuygulama.uygulama3
+
+fun main() {
+    val liste = listOf(3,5,7,9)
+    val top = toplamListe(liste)
+
+    println(" liste toplam : $top")
+}
+
+fun toplamListe(list: List<Int>): Int {
+    var top = 0
+    for (k in list) {
+        top += k
+
+
+    }
+    return top
+}
